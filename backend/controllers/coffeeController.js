ca05b4b8c1df2111dd9843e5ab11759089b3bddd
DESKTOP-F3BMJTI\nguye <hngu0053@student.monash.edu>
2023-12-24 17:33:50 +1100

    finish create coffee routes, controller, and model with get all coffee and add new coffee request

---
+++ backend/controllers/coffeeController.js
@@ -0,0 +1,43 @@
+const mongoose=require('mongoose');
+const Coffee=require('../models/Coffee');
+
+const getAllCoffeeInfo=async (req,res,next)=>{
+    try{
+        const foundedCoffee=await Coffee.find().lean();
+        if(!foundedCoffee?.length){
+            return res.status(400).json({message: "No Coffee Item is found"});
+        }
+        return res.json(foundedCoffee);
+    }
+    catch(err){
+        next(err);
+    }
+
+}
+
+const createNewCoffee=async(req,res, next)=>{
+    const {itemName, recipe, imageURL}=req.body;
+    if(!itemName||!recipe||!imageURL){
+        return res.json({message: "All field is required"});
+    }
+    const existItem= await Coffee.findOne({itemName:itemName}).lean().exec();
+    if(existItem){
+        return res.status(409).json({message:`An item name: ${itemName} is already created`});
+    }
+    const coffeeObject={
+        itemName:itemName,
+        recipe: recipe,
+        imageURL: imageURL
+    }
+    const coffee=await Coffee.create(coffeeObject);
+    if(coffee){
+        return res.status(201).json({message: `Item ${itemName} is successfuly added.`})
+    }
+    else{
+        return res.status(400).json({message: 'Invalid data recieved.'})
+    }
+}
+module.exports={
+    getAllCoffeeInfo,
+    createNewCoffee,
+};
