9b47904f97d1db5bafd72c5059f37b1e9ea08060
DESKTOP-F3BMJTI\nguye <hngu0053@student.monash.edu>
2023-12-24 22:18:12 +1100

    finish setting up redux store, reducers, adding error404 page when user enter wrong address link

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-
+import App from './components/App';
+import {Provider} from "react-redux"
+import { store } from './store/store';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Error404 from './components/Error404';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={store}>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App/>}/>
+      <Route path="/*" element={<Error404/>}/>
+    </Routes>
+    </BrowserRouter>
+  </Provider>
 );
---
+++ frontend/src/reducers/index.js
@@ -0,0 +1,5 @@
+import { combineReducers } from "@reduxjs/toolkit";
+import userReducer from "./userReducer";
+export const rootReducer=combineReducers({
+    user: userReducer
+})
