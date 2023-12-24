5d3c5d9ae39170186e731aa90073a5f034b83486
Hong Kien Nguyen <nguyenhongkien123tk@gmail.com>
2023-12-25 02:34:53 +1100

    added Staff section in the Homepage

---
+++ frontend/src/components/StaffCard.js
@@ -0,0 +1,25 @@
+import React from "react";
+export default function StaffCard(props){
+    const [isHover, setIsHover]=React.useState(false);
+    return (
+        <div onMouseEnter={()=>setIsHover(true)} onMouseLeave={()=>setIsHover(false)} style={{
+            display: "flex",
+            flexDirection:"column",
+            justifyContent:"center",
+            alignItems:"center"
+        }}>
+            <div style={{display:"flex", flexDirection:"column", width: "100%", height: "495px", backgroundImage: `url(${props.data.image})`, backgroundSize:"cover", backgroundRepeat:"no-repeat", placeContent:"flex-end"}}>
+                <div className="staff--card" style={{
+                    backgroundColor:"rgba(0,0,0,0.5)",
+                    color: "rgb(212, 193, 16)",
+                    visibility: isHover?"visible":"hidden"
+                }}>
+                    <h3 style={{fontFamily:"'Handlee', cursive"}}>{props.data.name}</h3>
+                    <h3 style={{fontFamily:"'Handlee', cursive"}}>Position: {props.data.role}</h3>
+                    <p style={{fontFamily:"'Handlee', cursive"}}>Description: {props.data.description}</p>
+                </div>
+            </div>
+            
+        </div>
+    )
+}
---
+++ frontend/src/documents/staffData.js
@@ -0,0 +1,26 @@
+import Arthur from "../images/Arthur.png"
+import Mary from "../images/Mary.png"
+import Billy from "../images/Billy.png"
+import Kieran from "../images/Kieran.png"
+
+export const staff=[{
+    name: "Arthur",
+    role: "Manager",
+    description:"",
+    image: Arthur
+},{
+    name: "Kieran",
+    role: "Barista",
+    description:"",
+    image: Kieran
+},{
+    name: "Billy",
+    role: "Barista",
+    description:"",
+    image: Billy
+},{
+    name: "Mary",
+    role: "Cashier",
+    description:"",
+    image: Mary
+},]
