4e0aa901b1c386d04400f5a9fe81ab7b0494c133
DESKTOP-F3BMJTI\nguye <hngu0053@student.monash.edu>
2023-12-24 23:33:10 +1100

    finish design navigation bar, adding banner, finish route for the front end

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,7 +1,16 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./Navbar";
+import HomePage from "./HomePage";
+import Error404 from "./Error404"
 function App() {
   return (
-    <div className="App">
-    </div>
+      <Routes>
+        <Route path="/" element={<Navbar/>}>
+          <Route path="" element={<HomePage/>}/>
+          <Route path="*" element={<Error404/>}/>
+        </Route>
+      </Routes>
   );
 }
 
---
+++ frontend/src/components/HomePage.js
@@ -0,0 +1,12 @@
+import "../css/HomePage.css"
+import banner from "../images/homepage_banner.png"
+export default function HomePage(){
+    return(
+        <div className="homepage">
+            <img src={banner} />
+            <div style={{height:'1000px'}}>
+
+            </div>
+        </div>
+    )
+}
