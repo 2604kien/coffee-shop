906a9e429de176b22b740ce3aead3dee2e831b4c
DESKTOP-F3BMJTI\nguye <hngu0053@student.monash.edu>
2023-12-25 15:37:48 +1100

    register new user now can interact with the server and the database

--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -1,5 +1,5 @@
 import { combineReducers } from "@reduxjs/toolkit";
 import userReducer from "./userReducer";
 export const rootReducer=combineReducers({
-    user: userReducer
+    users: userReducer
 })
--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,5 +1,7 @@
 import {configureStore} from "@reduxjs/toolkit";
 import { rootReducer } from "../reducers";
+
 export const store=configureStore({
-    reducer: rootReducer
+    reducer: rootReducer,
+    middleware: getDefaultMiddleware =>getDefaultMiddleware()
 })
