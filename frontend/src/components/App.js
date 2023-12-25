b52c7ccfb510d89535efd7d22146c134e6c23d81
DESKTOP-F3BMJTI\nguye <hngu0053@student.monash.edu>
2023-12-25 13:31:52 +1100

    finish Login component in front end

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -3,11 +3,13 @@ import { Routes, Route } from "react-router-dom";
 import Layout from "./Layout";
 import HomePage from "./HomePage";
 import Error404 from "./Error404"
+import Login from "./Login";
 function App() {
   return (
       <Routes>
         <Route path="/" element={<Layout/>}>
           <Route path="" element={<HomePage/>}/>
+          <Route path="login" element={<Login/>}/>
           <Route path="*" element={<Error404/>}/>
         </Route>
       </Routes>
