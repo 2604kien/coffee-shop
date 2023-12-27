baa8513c1c7874c9695c493c16f8240757bfe0b8
DESKTOP-F3BMJTI\nguye <hngu0053@student.monash.edu>
2023-12-27 12:13:36 +1100

    finish booking function

--- frontend/src/components/Booking.js
+++ frontend/src/components/Booking.js
@@ -1,7 +1,12 @@
 import React from "react";
 import "../css/Booking.css";
 import BookingPNG from "../images/Booking.png";
+import {useDispatch} from "react-redux";
+import { addNewBooking } from "../reducers/bookingReducer";
+import { useNavigate } from "react-router-dom";
 export default function Booking(){
+    const dispatch=useDispatch();
+    const navigate=useNavigate();
     const [formData, setFormData]=React.useState({
         dateTime:"",
         name:"",
@@ -10,6 +15,10 @@ export default function Booking(){
     });
     const handleSubmit=(e)=>{
         e.preventDefault();
+        dispatch(addNewBooking(formData)).then(()=>{
+            alert('We recieved your booking request and will contact with you shortly');
+            navigate('/');
+        })
     }
     const handleChange=(e)=>{
         const {name, value}=e.target
@@ -31,7 +40,7 @@ export default function Booking(){
                 <label htmlFor="name">Your Name:</label>
                 <input type="text" id="name" name="name" placeholder="Please enter your name." value={formData.name} onChange={handleChange} required/>
                 <label htmlFor="numPeople" >Number of People:</label>
-                <input type="number" id="numPeople" name="numPeople" defaultValue={1} value={formData.numPeople} onChange={handleChange} required/>
+                <input type="number" id="numPeople" name="numPeople" placeholder="Please enter number of people for reservation." value={formData.numPeople} onChange={handleChange} required/>
                 <label htmlFor="mobilePhone">Your Contact Number/Email Address:</label>
                 <input type="text" id="mobilePhone" name="mobilePhone" value={formData.mobilePhone} onChange={handleChange} required/>
                 <button>Submit</button>
--- frontend/src/reducers/bookingReducer.js
+++ frontend/src/reducers/bookingReducer.js
@@ -1,3 +1,21 @@
 import {createEntityAdapter, createSlice, createAsyncThunk} from "@reduxjs/toolkit";
-
+import axios from "axios";
 const bookingAdapter=createEntityAdapter();
+const initialState=bookingAdapter.getInitialState({});
+
+export const addNewBooking=createAsyncThunk('booking/addNewBooking', async(booking)=>{
+    const response=await axios.post("http://localhost:3500/booking", booking);
+    return response.data;
+})
+
+const bookingSlice=createSlice({
+    name:'booking',
+    initialState: initialState,
+    reducers:{},
+    extraReducers: (builder)=>{
+        builder.addCase(addNewBooking.fulfilled, (state, action)=>{
+            state.satus='succeeded';
+        })
+    }
+})
+export default bookingSlice.reducer;
