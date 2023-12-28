0048a1e3c80887fc4f1820e15b81340923d69829
DESKTOP-F3BMJTI\nguye <hngu0053@student.monash.edu>
2023-12-28 13:25:39 +1100

    added coffee recipe section

--- backend/routes/coffeeRoutes.js
+++ backend/routes/coffeeRoutes.js
@@ -1,6 +1,8 @@
 const express=require('express');
 const router=express.Router();
 const coffeeController=require('../controllers/coffeeController')
+const verifyJWT=require('../middleware/verifyJWT');
+router.use(verifyJWT);
 router.route('/')
     .get(coffeeController.getAllCoffeeInfo)
     .post(coffeeController.createNewCoffee)
