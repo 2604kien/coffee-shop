907407349a7fac31d89cb42de28951f8e9f601a2
DESKTOP-F3BMJTI\nguye <hngu0053@student.monash.edu>
2023-12-29 12:42:07 +1100

    finish user CRUD full stack

--- frontend/src/components/EditUser.js
+++ frontend/src/components/EditUser.js
@@ -1,24 +1,66 @@
 import React from "react";
 import BookingPNG from "../images/Booking.png";
 import { useSelector, useDispatch } from "react-redux";
-import { getUserById } from "../reducers/userReducer";
-import { useParams } from "react-router-dom";
+import { getUserById,editUser } from "../reducers/userReducer";
+import { useParams, useNavigate } from "react-router-dom";
+import moment from "moment";
 import "../css/EditUser.css"
 export default function EditUser(){
+    const navigate=useNavigate();
     const dispatch=useDispatch();
     const {id}=useParams();
     const userData=useSelector(state=> state.users.editUserData);
     const [formData, setFormData]=React.useState(userData);
-    const handleChange=()=>{
+    const handleChange=(e)=>{
+        const {name, value}=e.target;
+        if (name === "roles") {
+            const rolesArray = value.split(",").map((role) => role.trim());
+            setFormData((prev) => ({
+              ...prev,
+              [name]: rolesArray,
+            }));
+          } else {
+            setFormData((prev) => ({
+              ...prev,
+              [name]: value,
+            }));
+          }
+    }
 
+    const submitRegister=(e)=>{
+        e.preventDefault(); 
+        dispatch(editUser(formData)).then(()=>navigate('/all-users'))
     }
     React.useEffect(()=>{
-        dispatch(getUserById(id))
-    },[])
+        dispatch(getUserById(id)).then(()=>{
+            setFormData(userData);
+        })
+    },[dispatch, JSON.stringify(userData)])
     return(
         <div>
             <div className="coffee-recipe" style={{backgroundImage:`url(${BookingPNG})`}}></div>
-            <h1>This is Edit UserPage</h1>
+            <form onSubmit={submitRegister} className="coffee--form">
+                <fieldset style={{border: "3px solid rgb(212, 193, 16)", backgroundColor:"rgba(0,0,0,0.8", maxWidth:"300px"}}>
+                    <legend style={{backgroundColor:"rgba(0,0,0,0.5", fontSize:"3rem"}}>Edit User</legend>
+                  
+                    <label htmlFor="fullName">Full Name:</label>
+                    <input type="text" id="fullName" name="fullName" onChange={handleChange}  value={formData.fullName} required/>
+                    
+                    <label htmlFor="username">Username:</label>
+                    <input type="text" id="username" name="username" onChange={handleChange} value={formData.username} required/>
+
+                    <label htmlFor="roles">Roles: (use "," to separate the role)</label>
+                    <input type="text" id="roles" name="roles" onChange={handleChange} value={formData.roles}required/>
+
+                    <label htmlFor="refreshToken">Refresh Token:</label>
+                    <input type="text" id="refreshToken" name="refreshToken" value={formData.refreshToken} readOnly required/>
+
+                    <label htmlFor="createdDate">Created Date:</label>
+                    <input type="text" id="createdDate" name="createdDate" value={moment(Number(formData.createdDate)).format('h:mm A | DD/MM/YYYY')} readOnly required/>
+
+                    <button type="submit">Submit</button>
+                </fieldset>
+            </form>
         </div>
     )
 }
--- frontend/src/reducers/userReducer.js
+++ frontend/src/reducers/userReducer.js
@@ -3,18 +3,34 @@ import axios from "axios"
 const userAdapter=createEntityAdapter();
 const initialState=userAdapter.getInitialState({
     message:"",
-    editUserData:""
+    editUserData:{
+            _id: "",
+            username: "",
+            fullName: "",
+            roles: [],
+            createdDate: "",
+            refreshToken: "",
+      }
 });
 export const postNewUser= createAsyncThunk('users/postNewUser', async(user)=>{
     const response=await axios.post("http://localhost:3500/users", user);
     return response.data;
 })
-export const getUserById=createAsyncThunk('user/getUserById', async(id)=>{
+export const getUserById=createAsyncThunk('users/getUserById', async(id)=>{
     const response=await axios.get(`http://localhost:3500/users/${id}`);
+    return response.data;
+})
+export const editUser=createAsyncThunk('users/editUser', async(data)=>{
+    const response=await axios.put("http://localhost:3500/users", data);
     return response.data;
 })
 export const getAllUser=createAsyncThunk('users/getAllUser', async()=>{
     const response=await axios.get("http://localhost:3500/users");
+    return response.data;
+})
+export const deleteUser=createAsyncThunk('users/deleteUser', async(id)=>{
+    const response=await axios.delete(`http://localhost:3500/users/${id}`);
+    console.log(`http://localhost:3500/users/${id}`)
     return response.data;
 })
 const userSlice=createSlice({
