830018d98bae4b7eb2f923869809aa95ba0d3c8e
Hong Kien Nguyen <nguyenhongkien123tk@gmail.com>
2024-01-12 21:21:59 +1100

    disable devtool

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,6 +5,8 @@ import App from './components/App';
 import {Provider} from "react-redux"
 import { store } from './store/store';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import {disableReactDevTools} from "@fvilers/disable-react-devtools";
+if(process.env.NODE_ENV==='production') disableReactDevTools()
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
