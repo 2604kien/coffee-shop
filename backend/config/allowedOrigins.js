7b0747c5c962bbe64e007c37c5dd3a6418e94ed7
Hong Kien Nguyen <nguyenhongkien123tk@gmail.com>
2024-01-13 22:42:12 +1100

    add more origin to cors

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -1,6 +1,8 @@
 const allowedOrigins = [
     'http://localhost:3000',
-    'https://hiase.onrender.com'
+    'https://hiase.onrender.com',
+    'https://www.hiase.cafe/',
+    'https://hiase.cafe/'
 ]
 
 module.exports = allowedOrigins
