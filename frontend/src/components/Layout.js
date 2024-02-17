9ed0b0117f0a7f5b78d7ecf9e4e54a090829396f
DESKTOP-F3BMJTI\nguye <hngu0053@student.monash.edu>
2024-02-18 02:24:23 +1100

    fix infinite loading bug

--- frontend/src/components/Layout.js
+++ frontend/src/components/Layout.js
@@ -3,34 +3,45 @@ import Navbar from "./Navbar";
 import Footer from "./Footer";
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { refresh} from "../reducers/authReducer";
+import { refresh } from "../reducers/authReducer";
 import Loading from "./Loading";
-export default function Layout(){
-    const dispatch=useDispatch();
-    const token=useSelector(state=> state.auth.token);
-    const status=useSelector(state=>state.auth.status);
-    const isLogin=useSelector(state=>state.auth.isLogin);
-    React.useEffect(()=>{
-        if(!isLogin) dispatch(refresh())
-        const tokenExpirationThreshold = 3*60; 
-        let { exp } = token.length>0?JSON.parse(window.atob(token.split('.')[1])):"";
-        const intervalId = setInterval(()=>{
-                if (exp - Date.now() / 1000 < tokenExpirationThreshold) {
-                  // Dispatch the refreshAccessToken action
-                  
-                 dispatch(refresh());
-                 exp = {};
-                }
-            
-        }, 13*60*1000)
+
+export default function Layout() {
+    const dispatch = useDispatch();
+    const token = useSelector(state => state.auth.token);
+    const status = useSelector(state => state.auth.status);
+    const isLogin = useSelector(state => state.auth.isLogin);
+    const [loading, setLoading] = React.useState(status === "loading");
+
+    React.useEffect(() => {
+        if (!isLogin) {
+            dispatch(refresh());
+        }
+    }, [dispatch, isLogin]);
+
+    React.useEffect(() => {
+        const tokenExpirationThreshold = 3 * 60;
+        let exp = token.length > 0 ? JSON.parse(window.atob(token.split('.')[1])).exp : 0;
+        const intervalId = setInterval(() => {
+            if (exp - Date.now() / 1000 < tokenExpirationThreshold) {
+                // Dispatch the refreshAccessToken action
+                dispatch(refresh());
+            }
+        }, 13 * 60 * 1000);
+
         return () => clearInterval(intervalId);
-},[dispatch,token, isLogin]);
+    }, [dispatch, token]);
+
+    React.useEffect(() => {
+        setLoading(status === "loading");
+    }, [status]);
+
     return (
         <>
-            {status==="loading"&&<Loading/>}
-            <Navbar/>
-            <Outlet/>
-            <Footer/>
+            {loading && <Loading />}
+            <Navbar />
+            <Outlet />
+            <Footer />
         </>
-    )
+    );
 }
