94a394519e6ef8fcaac96475d99a277e0881af7a
Hong Kien Nguyen <nguyenhongkien123tk@gmail.com>
2024-02-18 02:24:23 +1100

    fix infinite loading bug

--- frontend/src/components/Loading.js
+++ frontend/src/components/Loading.js
@@ -1,12 +1,10 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import { useLocation } from "react-router-dom"
 import "../css/Loading.css"
 export default function Loading(){
-    const {pathname}=useLocation();
     return ReactDOM.createPortal(
         <div className="overlay">
-            {pathname==="/login"?<h3>Server may be slow sometimes, please be patient...</h3>: <></>}
+            <h3>Server may be slow sometimes, please be patient...</h3>
             <span className="loader"></span>
         </div>,
         document.body
