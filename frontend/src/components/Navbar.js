1a8a5735aeae52c0bdaf7ec7558d7ae6f902eefc
Hong Kien Nguyen <nguyenhongkien123tk@gmail.com>
2024-02-18 23:15:15 +1100

    finish integration of Auth0

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -3,13 +3,15 @@ import { useNavigate } from "react-router-dom";
 import "../css/Navbar.css";
 import {useSelector, useDispatch} from "react-redux";
 import { useLocation } from "react-router-dom";
-import { logout, resetState } from "../reducers/authReducer";
+import { useAuth0 } from "@auth0/auth0-react";
+import { logoutUser, resetState } from "../reducers/authReducer";
 export default function Navbar(){
     const navigate=useNavigate();
     const dispatch=useDispatch();
     const location=useLocation();
     const {pathname}=location;
-    const isAuthenticated=useSelector(state=> state.auth.isAuthenticated);
+    const { user, isAuthenticated, isLoading, logout } = useAuth0();
+    const isUserAuthenticated=useSelector(state=> state.auth.isAuthenticated);
     const isAdminAuthorized=useSelector(state=>state.auth.isAdminAuthorized);
     const [isScroll, setIsScroll]=React.useState(false);
     const [dropDown, setDropDown]=React.useState(false);
@@ -22,8 +24,9 @@ export default function Navbar(){
     },[])
     const handleLogout=async()=>{
         setDropDown(false);
-        await dispatch(logout());
+        await dispatch(logoutUser());
         await dispatch(resetState());
+        if(isAuthenticated) logout();
         navigate('/login');
     }
     return(
@@ -33,10 +36,10 @@ export default function Navbar(){
                 <li onClick={()=>{navigate('/')}} style={{fontWeight:"bold", fontSize:"2rem", width:"30%"}}>HIASE</li>
                 <div className="window--nav" >
                     <li onClick={()=>{navigate('/')}}>Home</li>
-                    {isAuthenticated && <li onClick={()=>navigate('/recipe')}>Coffe Recipe</li>}
-                    {isAdminAuthorized && isAuthenticated?<li onClick={()=>navigate('/booking-list')}>Booking List</li>: <li onClick={()=>navigate('/menu')}>Today's Menu</li>}
-                    {isAdminAuthorized && isAuthenticated?<li onClick={()=>navigate('/all-users')}>All Users</li>:<li onClick={()=>navigate('/booking')}>Booking</li>}
-                    {isAuthenticated?<li onClick={handleLogout}>Logout</li>: <li onClick={()=>navigate('/login')}>Login</li>}
+                    {isUserAuthenticated && <li onClick={()=>navigate('/recipe')}>Coffe Recipe</li>}
+                    {isAdminAuthorized && isUserAuthenticated?<li onClick={()=>navigate('/booking-list')}>Booking List</li>: <li onClick={()=>navigate('/menu')}>Today's Menu</li>}
+                    {isAdminAuthorized && isUserAuthenticated?<li onClick={()=>navigate('/all-users')}>All Users</li>:<li onClick={()=>navigate('/booking')}>Booking</li>}
+                    {isUserAuthenticated?<li onClick={handleLogout}>Logout</li>: <li onClick={()=>navigate('/login')}>Login</li>}
                 </div>
                 <div className="mobile--view">
                 <div className="mobile--nav--icon">
@@ -44,10 +47,10 @@ export default function Navbar(){
                 </div>
                 <div className={dropDown?"mobile--nav--container open":"mobile--nav--container"} style={{backgroundColor: window.scrollY===0 && pathname==="/"? "transparent":"white"}}>
                     <li onClick={()=>{navigate('/'); setDropDown(false)}}>Home</li>
-                    {isAuthenticated && <li onClick={()=>{navigate('/recipe'); setDropDown(false)}}>Coffe Recipe</li>}
-                    {isAdminAuthorized && isAuthenticated?<li onClick={()=>{navigate('/booking-list'); setDropDown(false)}}>Booking List</li>: <li onClick={()=>navigate('/menu')}>Today's Menu</li>}
-                    {isAdminAuthorized && isAuthenticated?<li onClick={()=>{navigate('/all-users'); setDropDown(false)}}>All Users</li>:<li onClick={()=>navigate('/booking')}>Booking</li>}
-                    {isAuthenticated?<li onClick={handleLogout}>Logout</li>: <li onClick={()=>{navigate('/login'); setDropDown(false)}}>Login</li>}
+                    {isUserAuthenticated && <li onClick={()=>{navigate('/recipe'); setDropDown(false)}}>Coffe Recipe</li>}
+                    {isAdminAuthorized && isUserAuthenticated?<li onClick={()=>{navigate('/booking-list'); setDropDown(false)}}>Booking List</li>: <li onClick={()=>navigate('/menu')}>Today's Menu</li>}
+                    {isAdminAuthorized && isUserAuthenticated?<li onClick={()=>{navigate('/all-users'); setDropDown(false)}}>All Users</li>:<li onClick={()=>navigate('/booking')}>Booking</li>}
+                    {isUserAuthenticated?<li onClick={handleLogout}>Logout</li>: <li onClick={()=>{navigate('/login'); setDropDown(false)}}>Login</li>}
                 </div>
                 </div>
             </ul>
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,13 +2,27 @@ import React from 'react';
 import "./css/index.css"
 import ReactDOM from 'react-dom/client';
 import App from './components/App';
+import { Auth0Provider } from '@auth0/auth0-react';
 import {Provider} from "react-redux"
 import { store } from './store/store';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import {disableReactDevTools} from "@fvilers/disable-react-devtools";
+import { getConfig } from "./config";
 if(process.env.NODE_ENV==='production') disableReactDevTools()
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const config = getConfig();
+const providerConfig = {
+  domain: config.domain,
+  clientId: config.clientId,
+  authorizationParams: {
+    redirect_uri: window.location.origin,
+    ...(config.audience ? { audience: config.audience } : null),
+  },
+};
 root.render(
+  <Auth0Provider
+  {...providerConfig}
+>
   <Provider store={store}>
     <BrowserRouter>
     <Routes>
@@ -16,4 +30,5 @@ root.render(
     </Routes>
     </BrowserRouter>
   </Provider>
+  </Auth0Provider>,
 );
